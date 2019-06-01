3a0c9944a7c3b99a660d2f1d7f7221465b28d7b5
Gergő Balogh <geryxyz@gmail.com>
2019-06-01 10:48:57 +0200

    adding logging support instead of print

--- dedup.py
+++ dedup.py
@@ -4,57 +4,80 @@ import sys
 import re
 import bibtexparser
 import functools
+import argparse
+import logging
+
 import pdb
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+logger.addHandler(handler)
 
 root_dir = sys.argv[1]
 exclude_pattern = None
+output_path = 'merged.bib'
+inspected_property = 'title'
+similarity_limit = .6
+
 if len(sys.argv) > 2:
     exclude_pattern = sys.argv[2]
+logger.info('searching Bibtex files in "{}", excluding "{}"'.format(root_dir, exclude_pattern))
 bibtex_files = glob2.glob(os.path.join(root_dir, '**', '*.bib'))
 dbs = {}
 for current_file in bibtex_files:
     if exclude_pattern is not None and re.search(exclude_pattern, current_file):
         continue
-    print("processing {}".format(current_file))
+    logger.debug('loading {}'.format(current_file))
     with open(current_file, 'r', encoding="utf8") as bibtex_file:
         dbs[current_file] = bibtexparser.load(bibtex_file)
+logger.info('{} files are loaded'.format(len(dbs)))
 
+logger.info('reading entries')
 merged = bibtexparser.bibdatabase.BibDatabase()
 for file, db in dbs.items():
-    print(file)
+    logger.debug('reading entries form {}'.format(file))
     entry_count = len(db.entries)
     for i, entry in enumerate(db.entries):
-        print("{:.2%}".format(i / entry_count))
+        logger.debug("{:.2%}".format(i / entry_count))
         merged.entries.append(entry)
+logger.info('{} entries load from {} databases'.format(len(merged.entries), len(dbs)))
 
 
 @functools.lru_cache()
-def parts_of(title):
-    return [part for part in re.split(r'\W', title) if part != '']
+def parts_of(value):
+    return [part for part in re.split(r'\W', value) if part != '']
 
 
 @functools.lru_cache()
-def jaccard_of(title, other_title):
-    title_parts = parts_of(title)
-    other_title_parts = parts_of(other_title)
-    common_parts = [part for part in title_parts if part in other_title_parts]
-    return len(common_parts) / (len(title_parts) + len(other_title_parts) - len(common_parts))
+def jaccard_of(value, other_value):
+    parts = parts_of(value)
+    other_parts = parts_of(other_value)
+    common_parts = [part for part in parts if part in other_parts]
+    return len(common_parts) / (len(parts) + len(other_parts) - len(common_parts))
 
 
+logger.info('filtering duplicated entries inspecting: similarity("{}") < {}'.format(inspected_property, similarity_limit))
 deduped = bibtexparser.bibdatabase.BibDatabase()
 entry_count = len(merged.entries)
+filtered_count = 0
 for i, entry in enumerate(merged.entries):
     id = entry['ID']
-    title = entry['title']
-    print("{:.2%} ({})".format(i / entry_count, len(deduped.entries)))
+    value = entry[inspected_property]
+    logger.debug("{:.2%} ({})".format(i / entry_count, len(deduped.entries)))
     if entry in deduped.entries:
         continue
     if deduped.entries:
-        jaccard_title = max([jaccard_of(title, other_entry['title']) for other_entry in deduped.entries])
-        if jaccard_title < .6:
+        similarity = max([jaccard_of(value, other_entry[inspected_property]) for other_entry in deduped.entries])
+        if similarity < similarity_limit:
             deduped.entries.append(entry)
+        else:
+            filtered_count += 1
     else:
         deduped.entries.append(entry)
+logger.info('{} ({:.2%}) entries are skipped'.format(filtered_count, filtered_count / entry_count))
 
-with open('merged.bib', 'w', encoding='utf8') as deduped_file:
+logger.info('saving debuped database into "{}"'.format(output_path))
+with open(output_path, 'w', encoding='utf8') as deduped_file:
     bibtexparser.dump(deduped, deduped_file)
